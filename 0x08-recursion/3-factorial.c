5caf73b0f2f67a658c83d2781eb7c20631f67148
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-07 23:30:24 +0000

    Calculates the factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+
+/**
+ * factorial - calculates the factorial of a number
+ * @n: the number
+ * Return: the factorial of the number.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
