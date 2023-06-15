044264d147b3333b22ea39f047e3cd744a0fd33d
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-15 12:20:29 +0000

    Added a header file

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
+void *_realloc(void *, unsigned int, unsigned int);
+
+
+
+
+#endif
