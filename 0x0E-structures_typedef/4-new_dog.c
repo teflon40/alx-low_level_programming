b92a740b8524ef9aa7bdef9e8a934e26959ab7af
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-19 15:15:36 +0000

    Wrote a function that creates a new struct variable

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,61 @@
+#include "dog.h"
+#include <stdlib.h>
+
+int _strlen(const char *);
+char *_strcpy(char *, const char *);
+/**
+ * my_dog - creates a new dog
+ * @name: a pointer to a string
+ * @age: a float parameter
+ * @owner: a pointer to a string
+ * Return: a struct type
+ * or NULL if the my_dog fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = (dog_t *) malloc(sizeof(dog_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->name = (char *) malloc(sizeof(char) * (_strlen(name) + 1));
+	if (ptr->name == NULL)
+		return (NULL);
+	ptr->owner = (char *) malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (ptr->owner == NULL)
+		return (NULL);
+	_strcpy(ptr->name, name);
+	_strcpy(ptr->owner, owner);
+	ptr->age = age;
+	return (ptr);
+}
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: a pointer to the string
+ * Return: the length of the string
+ */
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+		len++, s++;
+	return (len);
+}
+
+/**
+ * _strcpy - copies one string into another
+ * @dest: a pointer to the string to receive the copy
+ * @src: a pointer to the string to be copied
+ * Return: a pointer to the resulting string
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int len = 0;
+
+	while (*src != '\0')
+		dest[len++] = *src++;
+	dest[len] = '\0';
+	return (dest);
+}
