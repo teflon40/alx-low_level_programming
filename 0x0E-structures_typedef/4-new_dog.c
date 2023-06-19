2a7a126dac46ab99483b99f7b4dae97bb33eaf8a
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-19 16:09:00 +0000

    Creates a new struct variable

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
 
-	if (name == NULL || !age || owner == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 	ptr = (dog_t *) malloc(sizeof(dog_t));
 	if (ptr == NULL)
