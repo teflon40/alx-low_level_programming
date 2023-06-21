1473f36a8a620b329fe0a8981447d3f1754c6db4
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-21 16:46:26 +0000

    Added an if condition to check if array points to NULL

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
+	if (array == NULL)
+		return;
 	if (size > 0 && action)
 		for (; i < size; i++)
 			(*action)(array[i]);
