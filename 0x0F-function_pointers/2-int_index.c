8fc52e8f9b9c0194c34c8546933de574eadb350c
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-21 16:59:55 +0000

    Searches for an integer returns the index it was found at

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: the array to be searched
+ * @size: the size of array
+ *
+ * @cmp: pointer to the function that will perform the search
+ * Return: -1 if no element matches or size <= 0
+ * else return the index of the first element that met the search
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || size <= 0)
+		return (-1);
+
+	for (; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+	}
+	return (-1);
+}
