6d6c5901132c95353bdbb5ef8eae264ff59d6889
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-21 16:44:05 +0000

    Added a few prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,13 @@
 #ifndef FUNC_H
 #define FUNC_H
 
+#include <stddef.h>
+
 /* Prototypes */
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
-
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 
 
