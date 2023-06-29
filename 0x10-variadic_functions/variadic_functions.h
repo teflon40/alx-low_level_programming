b642625375d09a49ee6e35d656f64deca0c560c0
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-29 13:47:59 +0000

    Prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,5 +9,5 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *, const unsigned int, ...);
 void print_strings(const char *, const unsigned int, ...);
-
+void print_all(const char * const format, ...);
 #endif
