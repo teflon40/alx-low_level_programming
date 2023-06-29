88df66b71fd9ff02cc33c808acf8c9a8c78b2132
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-29 13:08:56 +0000

    Included a header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,6 @@
 /* Function prototypes */
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *, const unsigned int, ...);
-
+void print_strings(const char *, const unsigned int, ...);
 
 #endif
