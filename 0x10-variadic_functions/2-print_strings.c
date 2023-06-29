aceafb4d2718e2a02b1b3d3cd092852a650192e8
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-29 13:11:28 +0000

    Prints strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @str: a pointer to the string to be printed between strings
+ * @n: the number of strings passed
+ */
+void print_strings(const char *str, const unsigned int n, ...)
+{
+	unsigned int i = 1;
+	char *s;
+	va_list arg_l;
+
+	if (n)
+	{
+		va_start(arg_l, n);
+		while (i <= n)
+		{
+			s = va_arg(arg_l, char *);
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s", s);
+			if (str && i++ != n)
+				printf("%s", str);
+		}
+	}
+	putchar('\n');
+	va_end(arg_l);
+}
