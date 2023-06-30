937173ad0276dc30ef3bfaefe4d63fcf1a77857a
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-30 21:40:37 +0000

    Corrected "quoted string split across lines"

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@
  */
 void print_str(void)
 {
-	printf("You're beat! and yet, you must allow,"
-			"\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\
+			\nI bore my house upon my back!\n");
 }
