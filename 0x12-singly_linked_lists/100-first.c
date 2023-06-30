7e497a869af72acd6aff473fbe32076a2c4a9701
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-30 21:36:33 +0000

    Prints a string before the execution of the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * print_str - prints a string before the main function is executed.
+ */
+void print_str(void)
+{
+	printf("You're beat! and yet, you must allow,"
+			"\nI bore my house upon my back!\n");
+}
