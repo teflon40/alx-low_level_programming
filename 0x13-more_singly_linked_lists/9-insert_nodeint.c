a56eab8dd247a630e0b09fe8c44c2b112379ca24
teflon40 <frimpong.bernard3220@gmail.com>
2023-07-04 20:43:48 +0000

    Added a check for a NULL condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head,
 	listint_t *new_node, **temp_p;
 	unsigned int pos = 0;
 
-	if (!head || !*head)
+	if (!head)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
