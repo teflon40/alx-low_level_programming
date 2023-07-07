6754c1627c2c2fd5dd2ae6c4757eb2f342b80a23
teflon40 <frimpong.bernard3220@gmail.com>
2023-07-07 01:54:51 +0000

    return the value of a bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+char *create_num_str(unsigned long int, int *);
+/**
+ * get_bit - gets the value of a bit at a given index
+ * @n: an integer
+ * @index: the index of the bit to find.
+ * Return: the value of the bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	char *s;
+	int len, i;
+
+	i = 0;
+	s = create_num_str(n, &len);
+	for (len--; len >= 0; i++, len--)
+		if (i == (int) index)
+			return (s[len] - '0');
+	return (-1);
+}
+
+/**
+ * create_num_str - creates a string of 1 and 0 characters.
+ * @n: an integer to be converted to binary.
+ * @len: a pointer to the length of the string.
+ * Return: a pointer to the string created.
+ */
+char *create_num_str(unsigned long int n, int *len)
+{
+	char *num_str;
+	unsigned long num = n;
+	int pos;
+	*len = 1;
+
+	while (num / 2)
+	{
+		num /= 2;
+		*len += 1;
+	}
+	pos = *len;
+	num_str = malloc(sizeof(char) * (*len + 1));
+	if (num_str == NULL)
+		return (NULL);
+
+	num_str[pos--] = '\0';
+	while (n / 2)
+	{
+		num_str[pos--] = n % 2 + '0';
+		n /= 2;
+	}
+	num_str[pos] = n % 2 + '0';
+	return (num_str);
+}
