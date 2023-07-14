e779b19482a4da07f68c10b9dff3ba7fa940bd25
teflon40 <frimpong.bernard3220@gmail.com>
2023-07-14 16:34:28 +0000

    Added the prototype of _strlen

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,4 +12,16 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+/* Helper functions prototypes */
+int _strlen(const char *);
+
+/* defining macros SUCCESS and FAILURE */
+#ifndef SUCCESS
+#define SUCCESS 1
+#endif		/* end SUCCESS */
+
+#ifndef FAILURE
+#define FAILURE -1
+#endif		/* end FAILURE */
+
+#endif		/* end MAIN_H */
