f71d9b1d611c16f812753620faad470e80014b1f
teflon40 <frimpong.bernard3220@gmail.com>
2023-08-14 16:16:20 +0000

    removed test_git

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,9 +9,5 @@
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90)
-		return (1);
-	if (c >= 97 && c <= 122)
-		return (1);
-	return (0);
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90) ? 1 : 0);
 }
