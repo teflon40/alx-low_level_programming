68592a1e82b1b58916efce3e2185b994b27e9112
teflon40 <frimpong.bernard3220@gmail.com>
2023-08-23 09:05:23 +0000

    Correct spelling of 'struct' on line 19

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -2,6 +2,11 @@
 #define LISTS_H
 
 
+/* Library header files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -11,15 +16,15 @@
  * Description: doubly linked list mode structure
  *
  */
-typedef strut dlistint_s
+typedef struct dlistint_s
 {
 	int n;
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
-}
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-size_t dlistint_len(const dlintint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
