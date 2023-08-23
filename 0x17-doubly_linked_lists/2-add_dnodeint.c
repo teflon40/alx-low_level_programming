cf7f0520715ad5fa8c0eef0da41031b067aeeca5
teflon40 <frimpong.bernard3220@gmail.com>
2023-08-23 09:32:46 +0000

    Surround *head with parentheses

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	*head->prev = new;
+	(*head)->prev = new;
 	new->next = *head;
 	new->prev = NULL;
 	*head = new;
