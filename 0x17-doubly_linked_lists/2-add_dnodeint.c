572926814dc4c8fff1ce7c0bff6693790b17a792
teflon40 <frimpong.bernard3220@gmail.com>
2023-08-23 09:42:07 +0000

    Check if *head == NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,13 +11,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
+	if (head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
-	(*head)->prev = new;
 	new->next = *head;
 	new->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
 	*head = new;
 
 	return (new);
