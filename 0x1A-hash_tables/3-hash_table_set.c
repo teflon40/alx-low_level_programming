96beee3979ab5f36f5d0e793a93d7d6c8610075a
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-04 05:15:11 +0000

    Adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ *
+ * @ht: a pointer to the hash table
+ * @key: key
+ * @value: value associated with the key.
+ *
+ * Return: 1 on success, 0 on failue.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int hash;
+	hash_node_t *bucket;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	bucket = (hash_node_t *) malloc(sizeof(hash_node_t));
+	if (bucket == NULL)
+		return (0);
+	bucket->key = strdup(key);
+	bucket->value = strdup(value);
+	bucket->next = NULL;
+
+	hash = key_index((unsigned char *) key, ht->size);
+	if (ht->array[hash] == NULL)
+		ht->array[hash] = bucket;
+	else
+	{
+		bucket->next = ht->array[hash];
+		ht->array[hash] = bucket;
+	}
+	return (1);
+}
