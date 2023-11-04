95f6972be70dcfd3a215ea4005c2983bdebc0701
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-04 06:28:25 +0000

    Added a typedef

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,6 +34,7 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+typedef enum {PRINTED, NOT_PRINTED} printMode;
 /* ----------- Public Interface ----------- */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
