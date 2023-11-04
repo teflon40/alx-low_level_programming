beec0bd35c7ca7b02000903ea8d4f11bee6a957b
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-04 05:30:34 +0000

    Check for value == NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,6 +16,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
+	if (value == NULL)
+		return (0);
 
 	bucket = (hash_node_t *) malloc(sizeof(hash_node_t));
 	if (bucket == NULL)
