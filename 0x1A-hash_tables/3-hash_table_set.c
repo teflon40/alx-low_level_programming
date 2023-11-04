716ede6e2dfb12de222523620035a3366879ab29
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-04 05:39:08 +0000

    NULL check

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	bucket->value = strdup(value);
 	if (bucket->value == NULL)
-		return (NULL);
+		return (0);
 	bucket->next = NULL;
 
 	hash = key_index((unsigned char *) key, ht->size);
