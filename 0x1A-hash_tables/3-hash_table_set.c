79d11084123fefe86f0cfe5fd99f9e40d23fe42a
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-04 07:06:15 +0000

    Avoid duplication of keys

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,11 +12,25 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash = 0;
-	hash_node_t *bucket;
+	hash_node_t *bucket, *curr;
 
 	if (ht == NULL || key == NULL)
 		return (0);
 
+	hash = key_index((unsigned char *) key, ht->size);
+	curr = ht->array[hash];
+
+	while (curr)
+	{
+		if (strcmp(key, curr->key) == 0)
+		{
+			curr->value = strdup(value);
+			break;
+		}
+		curr = curr->next;
+	}
+	if (curr != NULL)
+		return (1);
 	bucket = (hash_node_t *) malloc(sizeof(hash_node_t));
 	if (bucket == NULL)
 		return (0);
@@ -24,13 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	bucket->value = strdup(value);
 	bucket->next = NULL;
 
-	hash = key_index((unsigned char *) key, ht->size);
 	if (ht->array[hash] == NULL)
 		ht->array[hash] = bucket;
-	else
-	{
-		bucket->next = ht->array[hash];
-		ht->array[hash] = bucket;
-	}
 	return (1);
 }
