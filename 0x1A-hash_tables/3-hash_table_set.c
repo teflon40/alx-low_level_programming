3c33cb5042e6360bfa6f86ad2461b19eaac58afd
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-07 19:35:53 +0000

    Hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,25 +12,13 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash = 0;
-	hash_node_t *bucket, *curr;
+	hash_node_t *bucket;
 
 	if (ht == NULL || key == NULL)
 		return (0);
 
 	hash = key_index((unsigned char *) key, ht->size);
-	curr = ht->array[hash];
 
-	while (curr)
-	{
-		if (strcmp(key, curr->key) == 0)
-		{
-			curr->value = strdup(value);
-			break;
-		}
-		curr = curr->next;
-	}
-	if (curr != NULL)
-		return (1);
 	bucket = (hash_node_t *) malloc(sizeof(hash_node_t));
 	if (bucket == NULL)
 		return (0);
@@ -40,5 +28,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[hash] == NULL)
 		ht->array[hash] = bucket;
+	else
+	{
+		bucket->next = ht->array[hash];
+		ht->array[hash] = bucket;
+	}
 	return (1);
 }
