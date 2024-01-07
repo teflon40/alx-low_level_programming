366e6a576e4f32d1c05f5e760907e4d8a4bff2ea
teflon40 <frimpong.bernard3220@gmail.com>
2024-01-07 16:59:48 +0100

    Error to stderr

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -52,7 +52,7 @@ void print_error(int status, const char *format, ...)
 
 	va_start(ap, format);
 	if (status == 97)
-		dprintf(STDOUT_FILENO, "%s", format);
+		dprintf(STDERR_FILENO, "%s", format);
 	else if (status == 100)
 		dprintf(STDERR_FILENO, format, va_arg(ap, int));
 	else
