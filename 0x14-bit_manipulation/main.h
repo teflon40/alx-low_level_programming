9ec83fad8627f1d88b148fcd469a4e3ae668ec84
teflon40 <frimpong.bernard3220@gmail.com>
2024-01-07 15:58:28 +0100

    Added a macro to set bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,9 @@
 #include <stdint.h>
 
 #define Bit_Value(x, y)		(((x) >> (y)) & 0x1)
-#define Bit_Width(n)		(sizeof((n)) * 8)
+#define Bit_Width(n)		(sizeof(n) * 8)
+#define Set_Bit(n, bit)		((n) | (1 << (bit)))
+
 /* Function prototypes */
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int);
