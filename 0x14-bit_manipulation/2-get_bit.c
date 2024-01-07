b4eb33d1b661f3aaaf2457674849fe1148cd62db
teflon40 <frimpong.bernard3220@gmail.com>
2024-01-07 14:53:32 +0100

    returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,71 +12,9 @@ int get_bin_len(unsigned long int);
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	char *num_str;
-	int len = get_bin_len(n);
-	int pos = len, i = 0;
+	uint8_t num_of_bits = Bit_Width(n);
 
-	if (len <= (int) index)
-		pos = len = index + 1;
-	num_str = malloc(sizeof(char) * (len + 1));
-	if (num_str == NULL)
+	if (index >= num_of_bits)
 		return (-1);
-	num_str = _memset(num_str, '0', len);
-
-	num_str[pos--] = '\0';
-	while (n / 2)
-	{
-		num_str[pos--] = n % 2 + '0';
-		n /= 2;
-	}
-	num_str[pos] = n % 2 + '0';
-
-	for (len--; len >= 0; i++, len--)
-		if (i == (int) index)
-		{
-			i = num_str[len];
-			free(num_str);
-			return (i - '0');
-		}
-	free(num_str);
-
-	return (-1);
-}
-
-/**
- * get_bin_len - creates a string of 1 and 0 characters.
- * @num: an integer to be converted to binary.
- * Return: the length of a number in binary.
- */
-int get_bin_len(unsigned long int num)
-{
-	int len = 1;
-
-	while (num / 2)
-	{
-		num /= 2;
-		len++;
-	}
-	return (len);
-}
-
-/**
- * _memset - fills the first n bytes of
- * memory area pointed to by s with a constant byte.
- * @s: points to a memory area.
- * @n: the number of bytes of s to be filled.
- * @b: the constant byte
- * Return: a pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *p = s;
-	unsigned int i = 0;
-
-	while (i < n)
-	{
-		*p = b;
-		i++, p++;
-	}
-	return (s);
+	return (Bit_Value(n, index));
 }
